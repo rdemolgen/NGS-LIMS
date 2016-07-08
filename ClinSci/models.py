7942ecce54bb9fa0b31e2a3d885c5eef2a5ebb86
Stuart_Cannon <stuartcannon@nhs.net>
2016-07-08 09:02:26 -0700

    more work on models

--- ClinSci/models.py
+++ ClinSci/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#
 
+#models each ngs batch
 class Batch(models.Model):
     #this all comes from the config.yaml file for each batch so assertions already in pipeline
     batch_primary_key = models.AutoField(primary_key=True)
@@ -26,6 +28,7 @@ class Batch(models.Model):
 
 #one_to_many with Sample on proviso that sample has a surrogate key
 
+#models each sample on each batch
 class Sample(models.Model):
     #autoincrementing primary key
     sample_primary_key = models.AutoField(primary_key=True)
@@ -38,41 +41,67 @@ class Sample(models.Model):
       #not as expected
     bam_file_path = models.CharField(max_length=200, blank=True)
 
-#sample_id (surrogate_key_made_by_database - autoincrement)
-#historic_sample_id
-#ex number
-#date
-#dob
+    #historic_sample_id
+    #ex number
+    #gender
+    #date
+    #dob
 
-#one to onewith metrics
+    #one to one with metrics
+    #one to one with ngs_test
 
+    def __str__(self):
+        return self.
+
+
+#models the ngs test used - method and version, if tngs the virtual panel phenotype, intervals covered
+#and intervals for variant calling
 class Ngs_test(models.Model):
+    Ngs_test_primary_key = models.AutoField(primary_key=True) 
     capture_method = models.CharField(max_length=200, blank=True)
     capture_version = models.CharField(max_length=200, blank=True)
     virtual_panel_phenotype = models.CharField(max_length=200, blank=True)
+    #format for gene panel = { 'panel_list' : '[gene_and_exon, gene_and_exon, ... ]'} 
+    virtual_panel_genes = models.JSONField()
     date_of_analysis = models.CharField(max_length=200, blank=True)
-
-class test_interval_list(models.Model):
-    interval_list_primary_key = models.AutoField(primary_key=True)
-    Ngs_test = models.ForeignKey(Ngs_test, on_delete=models.CASCADE)
+    covered_interval_list_by_phenotype = models.JSONField()
+    variant_calling_intervals = models.JSONField()
+    sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
     #to do this we need to read in the interval file as a JSON object (a dictionary essentially)
-    #comes in format: chrom,start,end,strand, gene_and_exon
-    #interval_dict = { 'gene_and_exon' : 'gene_and_exon'
+    #interval file comes in format: chrom,start,end,strand, gene_and_exon
+    #json object as a dictionary of dictionaries where each dictionary states the target gene and exon and the targets associated
+    #interval_dict = {{ 'gene_and_exon' : '<gene_and_exon>', 'bed_intervals' = ['<chrom>', 'start', 'end', 'strand']},{same_again}}
     #to minimise redeundancy, and only includethe targets relevant to the test we should only use the intervals of the test phenotype.
     #this will need to be put into the manager script an I think is e.g. v602_phenotype.code_metrics.interval_list
-    interval_list = models.JSONField()
-    variant_calling_intervals = models.JSONField()
 
+    def __str__(self):
+        return self.capture_version
+
+#class test_interval_list(models.Model):
+#    interval_list_primary_key = models.AutoField(primary_key=True)
+#    Ngs_test = models.ForeignKey(Ngs_test, on_delete=models.CASCADE)
+#    interval_list = models.JSONField()
+#    variant_calling_intervals = models.JSONField()
+
+#where overall is targeted capture and phen is virtual panel metrics
 class Sample_metrics(models.Model):
-    sample_metrics_path = models.CharField(max_length=200, blank=True)
+    overall_metrics_path = models.CharField(max_length=200, blank=True)
     phenotype_metrics_path = models.CharField(max_length=200, blank=True)
-    sample_pct_target_bases_20x = models.CharField(max_length=200, blank=True)
-    sample_pct_target_bases_30x = models.CharField(max_length=200, blank=True)
+    overall_pct_target_bases_20x = models.CharField(max_length=200, blank=True)
+    overall_pct_target_bases_30x = models.CharField(max_length=200, blank=True)
     phen_pct_target_bases_20x = models.CharField(max_length=200, blank=True)
     phen_pct_target_bases_30x = models.CharField(max_length=200, blank=True)
 
+    def __str__(self):
+        return self.phen_pct_target_bases_20x
+
+#alamut annotated
+#e.g. v501_nnnn_gender_phen.alamut.alltrans.txt
 class Variant_data():
-alamut annotated
+
+    def __str__(self):
+        return self.
+
 
 class Cnv_data():
 exomedepth_needs consideration stil
