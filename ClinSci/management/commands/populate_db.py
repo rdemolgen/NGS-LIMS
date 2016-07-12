3fccc35248e063a64569ee77b9270cda3c4667f5
Stuart_Cannon <stuartcannon@nhs.net>
2016-07-12 09:21:36 -0700

    stored sample list as JSON field

--- ClinSci/management/commands/populate_db.py
+++ ClinSci/management/commands/populate_db.py
@@ -82,7 +82,16 @@ class Command(BaseCommand):
                     dict[key] = value
                 #crete a Sample_list instance (as specified by the imported class from model.py) based on the
                 #parsed input data
-                list_entry = Sample_list(sequencing_panel_version = dict['sequencing_panel_version'], capture_number = dict['capture_number'], mody_number = dict['mody_number'], ex_number = dict['ex_number'], gender = dict['gender'], profile = dict['profile'], sample_type = dict['sample_type'], comments = dict['comments'])
+                list_entry = Sample_list(
+                    sequencing_panel_version = dict['sequencing_panel_version'],
+                    capture_number = dict['capture_number'],
+                    mody_number = dict['mody_number'],
+                    ex_number = dict['ex_number'],
+                    gender = dict['gender'],
+                    profile = dict['profile'],
+                    sample_type = dict['sample_type'],
+                    comments = dict['comments']
+                )
                 #prints to screen the ex number and panel version of the created Sample_list object
                 #(one for each line in the file) - this is possible because of the def __str__ function
                 print(list_entry.ex_number + ' ' + list_entry.sequencing_panel_version)
@@ -96,17 +105,47 @@ class Command(BaseCommand):
             #self.test_tngs_results_format(header_list) needs making
 
     def update_batch(self):
+        #update fields from the config file
         with open('/home/sjccannon/Documents/lims_project/test_project/data/tngs_output_files/2016-05-22_1234678/scripts/configuration/config.yaml', 'r') as input_file:
             for i in range(1):
-                _ = input_file.readline()
+                first_line = input_file.readline()
             loaded_file = yaml.load(input_file)
-#           print (loaded_file)
-            batch_details = Batch(
-                platform = loaded_file['sequencer_name'],
-                batch_id = loaded_file['batch_id'],
-                sample_list_path = loaded_file['sample_list_path']
+#            batch_details = Batch(
+#                platform = loaded_file['sequencer_name'],
+#                batch_id = loaded_file['batch_id'],
+#                sample_list_path = loaded_file['sample_list_path']
+#            )
+        #store the sample list as a JSON object {batch_id_from_yaml : [{'header_0' : 'value_0', 'header_1' :value_1 etc.},{'header_0' : 'value_0', 'header_1' :value_1 etc.}]} 
+        sample_list_location = loaded_file['sample_list_path']
+        with open('/home/sjccannon/Documents/lims_project/test_project/data/tngs_output_files/2016-05-22_1234678/scripts/configuration/sample_list_all.csv') as sample_list:
+            header_list = sample_list.readline().strip(' \n\t\r').split(',')
+            dict_for_json_dump = {loaded_file['batch_id'] : []}
+            for line in sample_list:
+                sample_dict = {}
+                strip_line = line.strip(' \n\t\r')
+                line = strip_line.split(',')
+                if line != ['']:
+                    for key, value in zip(header_list, line):
+                        sample_dict[key] = value
+                    for v in dict_for_json_dump.values():
+                        v.append(sample_dict)
+                        print(v)
+        batch_details = Batch(
+            platform = loaded_file['sequencer_name'],
+            batch_id = loaded_file['batch_id'],
+            sample_list_path = loaded_file['sample_list_path'],
+            sample_list = dict_for_json_dump
             )
-        print(batch_details.sample_list_path)
+        print(batch_details.sample_list)
+        batch_details.save()
+
+#import glob
+
+#def sample_model_update():
+#    path = '/home/sjccannon/Documents/lims_project/test_project/data/tngs_output_files/2016-05-22_1234678/assembly'
+#    bam_file = glob.glob(path + '/*realigned.bam') 
+#    print(bam_file)
+
 
     #handler function - handles the flags and executes the required functions
     def handle(self, *args, **options):
